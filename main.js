e989f01512612ce67429c7b938677923732b2713
Blake Renton <elblayko@gmail.com>
2015-09-28 20:18:59 -0500

    Large code revision to prevent spam to the Twitter API.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var Twitter = require('twitter');
-var mongoose = require('mongoose');
+var Twitter     = require('twitter');
+var mongoose    = require('mongoose');
+var q           = require('q');
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // Database configuration.
@@ -21,94 +22,121 @@ var User = mongoose.model('User', userSchema);
 require('./env.js');
 
 var client = new Twitter({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token_key: process.env.access_token_key,
-    access_token_secret: process.env.access_token_secret
+    consumer_key:           process.env.consumer_key,
+    consumer_secret:        process.env.consumer_secret,
+    access_token_key:       process.env.access_token_key,
+    access_token_secret:    process.env.access_token_secret
 });
 
-global.searchString    = "goin to the club";
+global.searchString    = "going to the club";
 global.replyString     = "Ain't none y'all old enough to go to the damn club!";
 
-/**
- *  @name           loop()
- *  @description    Makes an request to the Twitter API searching for tweets that match our search
- *                  criteria.  Send a reply to that user.
- */
+var getTweets = function() {
 
-var loop = function() {
+    var deferred = q.defer();
 
-    console.log('\n' + new Date() + ' Searching for tweets...');
+    console.log(new Date() + ' Searching for tweets.');
 
-    // Search for 3 tweets only.
-    client.get('search/tweets', {q: global.searchString, count: 3, result_type: 'recent'}, function (error, tweets, response) {
+    client.get('search/tweets', {
+        q: global.searchString,
+        count: 25,
+        result_type: 'recent'
+    }, 
 
+    function(error, tweets, response) {
         if (error) {
-            console.log('An error occured getting tweets. ' + error);
-            return;
+            return deferred.reject(error);
         }
 
         if (tweets.length == 0) {
-            console.log('No tweets found with search query.');
+            return deferred.reject('No tweets found relevant to search query.');
+        }
+
+        deferred.resolve(tweets);
+    });
+
+    return deferred.promise;
+ };
+
+var writeTweet = function(tweet) {
+
+    // Is this tweet a retweet?
+    if (tweet.retweeted_status != undefined) {
+        console.log('Tweet is a retweet.');
+        return false;
+    }
+
+    // Have we tweeted to this user already?
+    User.find({handle: tweet.user.screen_name}, function(error, data) {
+        if (error) {            
+            console.log('Already tweeted to user.');
+            return false;
+        }
+    });
+
+    // Create a new user, we won't bother them again.
+    var newUser = User({
+        handle: tweet.user.screen_name,
+        created_at: new Date()
+    });
+
+    newUser.save(function(error) {
+        if (error) {
+            console.error('Error writing user to database.');
+            return false;
+        }
+    });
+
+    // Post a tweet to the user.
+    client.post('statuses/update', {
+        status: '@' + tweet.user.screen_name + " " + global.replyString,
+        in_reply_to_status_id: tweet.id_str
+    },
+
+    function (error, tw, response) {
+        if (error) {
+            console.error('Error occured writing tweet: ' + tw.message);
             return;
         }
 
-        // We have tweets, let's make some replies.
-        tweets.statuses.forEach(function(tweet) {
-
-            var thisTweet = tweet;
-
-            // Don't reply to retweets.
-            if (tweet.retweeted_status == undefined) {
-
-                // Have we already tweeted to this user?
-                User.find({handle: tweet.user.screen_name}, function(error, data) {
-
-                    if (error) {
-                        console.log('Unable to connect to database.');
-                        return;
-                    }
-
-                    else if (data.length > 0) {
-                        console.log('Skipping (' + tweet.user.screen_name + ')');
-                        return;
-                    }
-
-                    // Create a new user, we don't want to tweet to them again in the future.
-                    var newUser = User({
-                        handle: tweet.user.screen_name,
-                        created_at: new Date()
-                    });
-
-                    newUser.save(function(error) {
-                        if (error) {
-                            console.log('Could not save user to database.');
-                            return;
-                        }
-                    });
-
-                    // Post a reply to that user.
-                    client.post('statuses/update', {
-                        status: '@' + tweet.user.screen_name + " " + global.replyString,
-                        in_reply_to_status_id: tweet.id_str
-                    },
-
-                    function(error, tweet, response) {
-                        if (error) {
-                            console.log('Error occured writing tweet.');
-                            return;
-                        }
-
-                        console.log('Tweeted to (' + thisTweet.user.screen_name + ') with no errors.');
-                    });
-                });                
-            }
-        });
+        global.currentTweetIndex++;
+        console.log('Tweeted to ' + tweet.user.screen_name);
     });
+
+    return true;
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // Main logic.
 
-loop();
-setInterval(loop, 300000);  // 5 minutes.
+getTweets().then(function(tweets) {
+
+    var tweets = tweets.statuses;
+    var currentTweetIndex = 0;
+
+    // Execute only once per minute.
+    setInterval(function() {
+
+        // Check that we haven't ran out of tweets.
+        // If we have, reset the index and get more.       
+        if (currentTweetIndex == tweets.length - 1) {
+            getTweets().then(function(tw) {
+                currentTweetIndex = 0;
+                tweets = tw.statuses;
+            }, console.log);
+        }
+
+        // Initialize the result of posting this tweet.
+        var result = false;
+
+        // Iterate over available tweets until we can get one that
+        // we haven't posted to, and one that isn't a retweet.
+        do {
+            result = writeTweet(tweets[currentTweetIndex]);
+            currentTweetIndex++;
+        }
+        while (result == false);
+
+    }, 15000);
+
+}, console.log);
